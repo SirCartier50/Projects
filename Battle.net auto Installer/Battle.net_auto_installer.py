5eb9d452a1f1565474d38429636478d6420aa766
SirCartier50 <146313078+SirCartier50@users.noreply.github.com>
2024-07-16 17:05:34 -0700

    Add files via upload

--- Battle.net auto Installer/Battle.net_auto_installer.py
+++ Battle.net auto Installer/Battle.net_auto_installer.py
@@ -1,6 +1,7 @@
 import pyautogui as pag
 import subprocess
 import time
+import sys
 #import os
 #drive = 'Y:'
 # directory = 'Battle.net'
@@ -54,9 +55,18 @@ def locate_start_install(c):
     except pag.ImageNotFoundException:
         print("couldn't find")
 
-def main():
+def test_func(c):
+    try:
+        x,y = pag.locateCenterOnScreen('C://Users//migno//OneDrive//Desktop//auto_images//test.jpg', confidence=c)
+        print(x,y, c)
+        time.sleep(0.5)
+        pag.moveTo(x,y)
+    except pag.ImageNotFoundException:
+        print('couldnt find the x')
+
+def battle_net():
     temp=0
-    while temp<2:
+    while temp<10:
         #open battle.net
         process=subprocess.Popen('Y://Battle.net//Battle.net Launcher.exe')
         pag.FAILSAFE = True
@@ -64,36 +74,59 @@ def main():
         #wait until main window appears
         #add a case that the main window does open in which still at login waiting for password or busy queue or bad load up
         c = 0.9
-        x = 0
+        counter = 0
         #1 indicates install and 2 indicates update
         threshold = 0.4
         while True:
             try:
-                x,y = pag.locateCenterOnScreen('C://Users//migno//OneDrive//Desktop//auto_images//cod_image.png', confidence=c)
+                x,y = pag.locateCenterOnScreen('C://Users//migno//OneDrive//Desktop//auto_images//test.jpg', confidence=c)
                 print(x,y, c)
                 #find the cod icon and click it
                 time.sleep(0.5)
                 pag.click(x,y)
-                print("clicked cod")
-                x = 1
-                break
+                print("what's new popped up")
             except pag.ImageNotFoundException:
                 try:
-                    x,y = pag.locateCenterOnScreen('C://Users//migno//OneDrive//Desktop//auto_images//cod_upd.jpg', confidence=c)
+                    x,y = pag.locateCenterOnScreen('C://Users//migno//OneDrive//Desktop//auto_images//cod_image.png', confidence=c)
                     print(x,y, c)
+                    #find the cod icon and click it
                     time.sleep(0.5)
-                    pag.moveTo(x,y)
-                    x = 2
+                    pag.click(x,y)
+                    print("clicked cod")
+                    counter = 1
                     break
                 except pag.ImageNotFoundException:
-                    print('sleep')
-                    time.sleep(1)
-                    print('wake up')
+                    try:
+                        x,y = pag.locateCenterOnScreen('C://Users//migno//OneDrive//Desktop//auto_images//cod_upd.jpg', confidence=c)
+                        print(x,y, c)
+                        time.sleep(0.5)
+                        pag.moveTo(x,y)
+                        counter = 2
+                        break
+                    except pag.ImageNotFoundException:
+                        try:
+                            time.sleep(0.5)
+                            x,y = pag.locateCenterOnScreen('C://Users//migno//OneDrive//Desktop//auto_images//file_e.jpg', confidence=c)
+                            pag.click(x,y)
+                            print(x,y, c)
+                            while True:
+                                try:
+                                    x,y = pag.locateCenterOnScreen('C://Users//migno//OneDrive//Desktop//auto_images//file_e.jpg', confidence=c)
+                                    pag.click(x,y)
+                                    print(x,y, c)
+                                except pag.ImageNotFoundException:
+                                    break
+                            counter = 3
+                            break
+                        except pag.ImageNotFoundException:
+                            print('sleep')
+                            time.sleep(1)
+                            print('wake up')
 
         #PROBLEM:might need to give time between clicks because it clicked but didn't pull cod window
         time.sleep(0.5)
 
-        if(x == 1):
+        if(counter == 1):
             #if there's install button press it else if there's pause button press it (maybe keep checking for it)
             locate_install(c)
             
@@ -108,10 +141,14 @@ def main():
 
             #hit start install
             locate_start_install(c)
-        elif(x == 2):
+        elif(counter == 2):
             locate_update(c)
-
+        elif(counter == 3):
+            #maybe terminate
+            return 1
         #wait until either it's done or not responding
+        #add a chewck for file_e and keep deleting those then if there's still not battle.net responding then press pause button then wait again for not responding
+        test_func(c)
         while True:
             try:
                 pag.locateCenterOnScreen('C://Users//migno//OneDrive//Desktop//auto_images//a.jpg', confidence=c)
@@ -123,7 +160,13 @@ def main():
                 time.sleep(5)
         print("done")
         temp+=1
-        print(temp)
+        print(temp)  
 
+def main():
+    if len(sys.argv) > 1 or len(sys.argv) < 1:
+        print("not valid input")
+        exit(1)
+    if(sys.argv[0] == "battle.net"):
+        battle_net()
 if __name__ == "__main__":
     main()
