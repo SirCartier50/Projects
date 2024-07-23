6896cd61119bedfa980f794b05aab67ebf78b0b6
SirCartier50 <146313078+SirCartier50@users.noreply.github.com>
2024-07-23 16:03:49 -0700

    Update httpserver.h

---
+++ ConcurConnect/httpserver.h
@@ -0,0 +1,22 @@
+#ifndef _HTTPSERVER
+#define _HTTPSERVER
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdbool.h>
+#include <regex.h>
+#include <limits.h>
+#include <dirent.h>
+#include "asgn2_helper_funcs.h"
+#include "protocol.h"
+#include <errno.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <pthread.h>
+#include "queue.h"
+#include "rwlock.h"
+#include "list.h"
+#endif
